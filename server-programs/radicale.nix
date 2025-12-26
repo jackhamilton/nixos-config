5ce5f9af71791a242470924b777279a4cc3f9f3d
Jack Hamilton <jackham800@gmail.com>
2025-12-26 13:24:04 -0800

    Upd

--- server-programs/radicale.nix
+++ server-programs/radicale.nix
@@ -6,7 +6,8 @@
         server.hosts = [ "0.0.0.0:5232" ];
         auth = {
           type = "htpasswd";
-          htpasswd_filename = "/etc/nixos/.htpasswd";
+          # htpasswd -c -B /etc/radicale/users jack
+          htpasswd_filename = "/etc/radicale/users";
           # hash function used for passwords. May be `plain` if you don't want to hash the passwords
           htpasswd_encryption = "bcrypt";
         };
