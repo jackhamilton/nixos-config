4bed21a958fe7d163df7304f8f43b489ca2dae9f
Jack Hamilton <jackham800@gmail.com>
2026-04-22 21:03:26 -0500

    update qmk tools

--- desktop.nix
+++ desktop.nix
@@ -10,6 +10,15 @@
 
   services.flatpak.enable = true;
   services.ratbagd.enable = true;
+  
+  environment.systemPackages = [
+    pkgs.qmk-udev-rules
+  ];
+
+  services.udev.packages = [
+    pkgs.qmk-udev-rules
+  ];
+
   systemd.services.flatpak-repo = {
     wantedBy = [ "multi-user.target" ];
     path = [ pkgs.flatpak ];
